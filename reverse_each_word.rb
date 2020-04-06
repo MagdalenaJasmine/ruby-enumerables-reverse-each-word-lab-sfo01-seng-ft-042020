df39d7a986f99b6da4bbea9a25817edf1187c393
Magdalena Cooper <magdalenacooper92@gmail.com>
2020-04-06 15:31:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,14 @@
+def reverse_each_word(string)
+  new_string= ''
+  array = string.split(/ /)
+  array.each do
+    |string| string.reverse 
+    new_string = new_string.strip + (' ') + string.reverse 
+  end 
+  return new_string
+end 
 
-
-
-def reverse_each_word(array)
+def reverse_each_word_new(array)
   array = string.split(/ /)
   array.collect { |string| string.reverse}
 end 
